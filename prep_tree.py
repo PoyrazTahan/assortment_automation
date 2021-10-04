701edbca72fb44dd2e2ff0e091d78de33ffed1f4
Poyraz Tahan <poyraz_tahan@mckinsey.com>
2021-10-04 14:03:01 +0300

    test commit - environment setup

--- prep_tree.py
+++ prep_tree.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 # %%
-folder = f'..//data'
+folder = f'C://Users//Poyraz Tahan//Box Sync//_Personal//6_RAD//skin//data'
 """"
 Regular Pipeline:
     Preliminary:
